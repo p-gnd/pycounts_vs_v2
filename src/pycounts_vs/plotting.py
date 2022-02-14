5aa41c6b2ac803ceae9fc46dcacd368471246a4d
Tomas Beuzen <tomas.beuzen@gmail.com>
2022-02-14 20:06:26 +1100

    build: added dev dependencies for docs

--- src/pycounts_vs/plotting.py
+++ src/pycounts_vs/plotting.py
@@ -1,7 +1,27 @@
 import matplotlib.pyplot as plt
 
 def plot_words(word_counts, n=10):
-    """Plot a bar chart of word counts."""
+    """Plot a bar chart of word counts.
+
+    Parameters
+    ----------
+    word_counts : collections.Counter
+        Counter object of word counts.
+    n : int, optional
+        Plot the top n words. By default, 10.
+
+    Returns
+    -------
+    matplotlib.container.BarContainer
+        Bar chart of word counts.
+
+    Examples
+    --------
+    >>> from pycounts.pycounts import count_words
+    >>> from pycounts.plotting import plot_words
+    >>> counts = count_words("text.txt")
+    >>> plot_words(counts)
+    """
     top_n_words = word_counts.most_common(n)
     word, count = zip(*top_n_words)
     fig = plt.bar(range(n), count)
@@ -9,8 +29,3 @@ def plot_words(word_counts, n=10):
     plt.xlabel("Word")
     plt.ylabel("Count")
     return fig
-
-from pycounts_vs.pycounts_vs import count_words
-from pycounts_vs.plotting import plot_words
-counts = count_words("zen.txt")
-fig = plot_words(counts, 10)
